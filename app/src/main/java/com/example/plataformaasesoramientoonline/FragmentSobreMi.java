75cfc8644132d8b8a38e18112092b8c2a345136f
alu20908719v <davidprades8@gmail.com>
2019-01-16 18:20:32 +0100

    Sobre Mi Casi terminado

--- app/src/main/java/com/example/plataformaasesoramientoonline/FragmentSobreMi.java
+++ app/src/main/java/com/example/plataformaasesoramientoonline/FragmentSobreMi.java
@@ -1,27 +1,25 @@
 package com.example.plataformaasesoramientoonline;
 
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FragmentSobreMi extends Fragment {
+public class FragmentSobreMi extends Fragment implements View.OnClickListener {
 
-
+    public Button buttonYoutube;
+    public Button buttonInstagram;
+    public Button buttonTwitter;
+    public Button buttonFacebook;
     public FragmentSobreMi() {
         // Required empty public constructor
 
@@ -33,27 +31,26 @@ public class FragmentSobreMi extends Fragment {
 
         View rootView = inflater.inflate(R.layout.fragment_sobre_mi, container, false);
         // Inflate the layout for this fragment
+        buttonYoutube  = rootView.findViewById(R.id.buttonYoutube);
+        buttonInstagram  = rootView.findViewById(R.id.buttonInstagram);
+        buttonTwitter  = rootView.findViewById(R.id.buttonTwitter);
+        buttonFacebook  = rootView.findViewById(R.id.buttonFacebook);
 
-        Button button = rootView.findViewById(R.id.buttonYoutube);
+        buttonYoutube.setBackground(getResources().getDrawable(R.drawable.youtube));
+        buttonInstagram.setBackground(getResources().getDrawable(R.drawable.botoninstagram));
+        buttonTwitter.setBackground(getResources().getDrawable(R.drawable.twitter));
+        buttonFacebook.setBackground(getResources().getDrawable(R.drawable.facebook));
 
-        Log.d("canal", "fuera");
-       button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d("canal", "dentro");
-               irAYoutube();
-            }
-        });
+        buttonYoutube.setOnClickListener(this);
 
-
-        return inflater.inflate(R.layout.fragment_sobre_mi, container, false);
+        return rootView;
     }
 
 
-    public void irAYoutube(){
-        Intent i = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com"));
+    @Override
+    public void onClick(View v) {
+        Intent i = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/channel/UCnpCOBcIaj_zAhl19j2ex7Q"));
         startActivity(i);
     }
-
 
 }
